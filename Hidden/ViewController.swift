27f5aa2091e08b61fcdfa48d0c48e629a17c966f
Henry Tran <qnt@duke.edu>
2016-09-02 13:49:26 -0400

    Initial Commit

---
+++ Hidden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hidden
+//
+//  Created by Minh Tran on 9/2/16.
+//  Copyright © 2016 Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
